7ed25ef78b75f1faec1eaabc54fce6118b19acfe
ReniNrhyn <nurhayanireni73@gmail.com>
2024-06-01 23:23:51 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      width: {
+        '900px': '900px',
+        '400px': '400px',
+      }
+    },
+  },
+  plugins: [],
+}
